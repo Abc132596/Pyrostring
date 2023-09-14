1e1303985df19729f92613f6312d01ce2a5e9bc5
Abc132596 <126934468+Abc132596@users.noreply.github.com>
2023-09-14 19:18:18 +0530

    config.py

--- Pyrostring/config.py
+++ Pyrostring/config.py
@@ -2,6 +2,6 @@
 CONFIG GENERATOR
 """
 
-API_ID   : int = 101010
-API_HASH : str = 'abcdefghijklmnopqrstuv'
+API_ID   : int = 21543397
+API_HASH : str = '87579ffc7be4d3ababdc07cbbc2e8dc4'
 #you can get this on my.telegram.org
